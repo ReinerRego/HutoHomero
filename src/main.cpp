b77112d49cb228f896ff11f3747fbc849f3b98b7
ReinerRego <regokoppany@gmail.com>
2023-10-29 22:04:09 +0100

    removed brzoi2c and changed ip to aws

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int previousMillis = 0;    // Store the last time a task was executed
 
 String hostnamePrefix = "CharterHutohomero-";
 String combinedHostname = "default";
-const char *serverAddress = "192.168.0.11";
+const char *serverAddress = "51.20.83.151";
 ESP8266WebServer server(80);
 
 bool loadSettings(char *data);
